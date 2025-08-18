565b73bb8f5260fdc2c985ffe47e10cf745c4325
Ophélie Sacré <ophelie.sacre@outlook.be>
2025-08-18 16:04:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Menu</title>
+</head>
+<body>
+    <video autoplay muted loop id="fond-menu">
+    <source src="decor/Decor_prairie_etherea.mp4" type="video/mp4">
+</video>
+    <div class="menu">
+        <div class="logo-menu"><img src="logo/logoEtherea_ombre.png" alt=""></div>
+        <div class="content-menu">
+            <a href="#" id="play" class="bouton-menu"><h1>Play</h1></a>
+            <a id="level" class="bouton-menu"><h1>Level</h1></a>
+            <a id="settings" class="bouton-menu"><h1>Settings</h1></a>
+            <a id="quit" class="bouton-menu"><h1>Quit</h1></a>
+    </div>
+</body>
+</html>
