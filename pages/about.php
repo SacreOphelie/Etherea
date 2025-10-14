20eb2ac0f143cb08488df8d89fbb4e6b4961b7b5
Ophélie Sacré <ophelie.sacre@outlook.be>
2025-10-14 14:20:05 +0200

    about

--- pages/about.php
+++ pages/about.php
@@ -3,11 +3,12 @@
         <source src="decor/about.webm" type="video/mp4">
     </video>
     <div class="content-about">
-        <h1>Welcome to Etherea !</h1>
-        <p>Dive into a 2D adventure where friendship, courage, and magic come together. Follow Ava, a young heroine from Terraflora, who has always been fascinated by the legend of Etherea. <strong>A mysterious world where magic truly exists.</strong></p>
-
-        <P>On her 18th birthday, the village sage entrusts her with <strong>an ancient artifact,</strong> capable of revealing the fragments of a legendary key. With her friends by her side, Ava embarks on a perilous journey through diverse and enchanting worlds, seeking to unlock the gates of Etherea and uncover the secrets they hold.</P>
-
-        <p>If you want to discover more, head to the left: there you’ll find <strong>the characters and the different worlds of Etherea.</strong> Already convinced? <strong>Just click the Download button !</strong></p>
+        <div class="texte-about">     
+            <p>Dive into a 2D adventure where friendship, courage, and magic come together. Follow Ava, a young heroine from Terraflora, who has always been fascinated by the legend of Etherea. <strong>A mysterious world where magic truly exists.</strong></p>
+    
+            <P>On her 18th birthday, the village sage entrusts her with <strong>an ancient artifact,</strong> capable of revealing the fragments of a legendary key. With her friends by her side, Ava embarks on a perilous journey through diverse and enchanting worlds, seeking to unlock the gates of Etherea and uncover the secrets they hold.</P>
+    
+            <p>If you want to discover more, head to the left: there you’ll find <strong>the characters and the different worlds of Etherea.</strong> Already convinced? <strong>Just click the Download button !</strong></p>
+        </div>
     </div>
 </div>
